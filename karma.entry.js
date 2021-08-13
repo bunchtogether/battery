c7a65b74f1c0a8885534f23fbcc8f4d5621b339d
John Wehr <johnwehr@bunchenterprise.com>
2021-08-13 13:26:35 -0400

    Re-add commented test

--- karma.entry.js
+++ karma.entry.js
@@ -2,6 +2,6 @@
 
 import './tests/database.test.js';
 import './tests/queue.test.js';
-//import './tests/worker.test.js';
+import './tests/worker.test.js';
 import './tests/fuzz.test.js';
 import './tests/queue-watcher.test.js';
